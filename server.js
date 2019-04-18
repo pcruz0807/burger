0437e654c76ee296ac842ffa83f31dda23032341
pcruz0807 <codecruz86@gmail.com>
2019-04-17 21:53:36 -0700

    push app to heroku

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var express = require("express")
-var app = express()
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-/*
-app.get('/', function (req, res) { 
-    res.send('Hello World')
-})
+var port = process.env.PORT || 3000;
 
-app.listen(3000)
-*/
+var app = express();
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
+console.log("Server is listening on " + port + ".......");
